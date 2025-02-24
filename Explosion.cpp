da05eb7c5925e21c66ae1bd2cf0aa8d408c54a6c
kdowd <kevin.dowd@gmail.com>
2025-02-25 11:46:16 +1300

    add in function for message and mouse

--- Explosion.cpp
+++ Explosion.cpp
@@ -14,13 +14,25 @@ const int MAX_COLORS_COUNT = 21;
 #define NUM_FRAMES_PER_LINE     5
 #define NUM_LINES               4
 
+void debugMousePos() {
 
+	if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+		Vector2 ballPosition = GetMousePosition();
+		ballPosition = GetMousePosition();
+		cout << ballPosition.x << " -- " << ballPosition.y << endl;
+	}
+
+};
+
+void showMessage() {
+	if (IsKeyDown(KEY_SPACE)) {
+		DrawText("Welcome To My Game, Suckers !!!!", 18, GetScreenHeight() - 30, 20, GOLD);
+		DrawFPS(10, 10);
+	}
+}
 
 int main()
 {
-	std::cout << "Hello World!\n";
-
-
 	Color BackGroundColour = { 155,25,140,255 };
 	SetTraceLogLevel(TraceLogLevel::LOG_NONE);
 
@@ -30,7 +42,7 @@ int main()
 
 	Texture2D egg = LoadTexture("pngegg.png");
 
-	//192
+	//192 for this image
 	float frameWidth = (float)(egg.width / NUM_FRAMES_PER_LINE);
 	//192
 	float frameHeight = (float)(egg.height / NUM_LINES);
@@ -42,21 +54,14 @@ int main()
 
 	bool active = false;
 	int framesCounter = 0;
-	Vector2 ballPosition = GetMousePosition();
+
 
 	while (WindowShouldClose() == false) {
-		ballPosition = GetMousePosition();
-
-		if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
-			cout << ballPosition.x;
-		}
-
-		if (IsKeyDown(KEY_SPACE)) {
-			DrawText("Welcome To My Game, Suckers !!!!", 18, GetScreenHeight() - 30, 20, GOLD);
-			DrawFPS(10, 10);
-		}
+		debugMousePos();
+		showMessage();
 
 		framesCounter++;
+
 
 		if (framesCounter > 2)
 		{
@@ -78,13 +83,14 @@ int main()
 		}
 
 		BeginDrawing();
-		//DrawTexture(egg, screenWidth / 2 - egg.width / 2, screenHeight / 2 - egg.height / 2, WHITE);
-		DrawTextureRec(egg, frameRec, position, WHITE);
 
+		DrawTextureRec(egg, frameRec, position, WHITE);
 		frameRec.x = frameWidth * currentFrame;
 		frameRec.y = frameHeight * currentLine;
+
 		EndDrawing();
 
+		// clear game area and draw background colour
 		ClearBackground(BackGroundColour);
 	}
 
